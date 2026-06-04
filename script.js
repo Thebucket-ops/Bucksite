a0f92aaebd6197269eb25446afbd2919de20137e
Thebucket-ops <the.bucket020@gmail.com>
2026-06-04 19:07:23 +0200

    had fun in js

---
+++ script.js
@@ -0,0 +1,30 @@
+//console.log('Hello there')
+
+//window.alert('this are like those thinghies that tell you if you quit the site will lose progress ')
+//window.prompt() does the same but asks for an input
+//let x; declaration
+//x=100; assignment or let x =132; (can put it in console.log easy peasy (or with ${variable name}))
+//html:label lets me input stuff
+//      button used to submit
+//strings with "" 
+//booleans are just normal 
+// to convert variables make this
+// varname= Number/String/etc.(varname);
+
+scheduler.postTask
+document.getElementById("splash").textContent="make random phrases come out";
+
+document.getElementById("seal").onclick = function(){
+window.alert('you clicked a sea lion, he happy :3')
+}
+//TODO make so that when clicked shows a gif of the sea lion dancing cuz hes cute
+
+document.getElementById("ominousbucket").onclick = function(){
+window.alert('sorry, this is still a work in progress -Buck')
+}
+//REMEMBER remove this when new project is ready ;)
+
+//changes content that has a specific id (can do dat with classes too) or use ${}
+//.onclick starts function as 
+//user interface how to set it up idk
+h
